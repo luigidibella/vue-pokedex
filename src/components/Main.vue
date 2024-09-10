85ee0f9ebf3c1fd09a3a28a8e839e65bf2638dc9
LuigiDiBella <lykos.omega@gmail.com>
2024-09-11 01:49:19 +0200

    [add] Main

---
+++ src/components/Main.vue
@@ -0,0 +1,77 @@
+<script>
+import SearchBar from './partials/SearchBar.vue';
+import Display from './partials/Display.vue';
+import Stats from './partials/Stats.vue';
+import Team from './partials/Team.vue';
+
+export default {
+  components: {
+    SearchBar,
+    Display,
+    Stats,
+    Team,
+  }
+
+}
+</script>
+
+<template>
+  <main>
+    <div class="my-container flex-sm-row vh-100">
+      <div class="my-col">
+        <SearchBar />
+        <Display />
+        <Stats />
+      </div>
+      <div class="my-col">
+        <Team />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style>
+main {
+  width: 100vw;
+  height: 100vh;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  background-color: #ced4da;
+}
+.my-container {
+  width: 800px;
+  height: 500px;
+  
+  display: flex;
+  flex-direction: column;
+  
+  border: 1px solid black;
+  border-radius: 10px;
+  background-color: #7e0000;
+  
+  padding: 15px;
+  gap: 15px;
+}
+
+.my-col {
+  width: 100%;
+  height: 100%;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  
+  border: 1px solid white;
+  background-color: #b60b0c;
+}
+
+@media (min-width: 576px) {
+  .my-container {
+    max-height: 500px;
+  }
+}
+</style>
